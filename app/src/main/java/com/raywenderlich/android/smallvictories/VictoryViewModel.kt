ac2c6b48e8ebf7016c21d4579ae7117529d7532a
Tori Lew <torirlew@gmail.com>
2023-05-29 12:20:17 -0700

    Green - incrementVictoryCountReturnsUpdatedCount test. This and previous tests in commits do not need refactoring

--- app/src/main/java/com/raywenderlich/android/smallvictories/VictoryViewModel.kt
+++ app/src/main/java/com/raywenderlich/android/smallvictories/VictoryViewModel.kt
@@ -54,6 +54,7 @@ class VictoryViewModel : ViewModel() {
   fun incrementVictoryCount() {
     val newCount = repository.getVictoryCount() + 1
     repository.setVictoryCount(newCount)
+    viewState.value = VictoryUiModel.CountUpdated(newCount)
   }
 
   fun reset() {
